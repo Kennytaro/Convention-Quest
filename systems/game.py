12266b44576990d07957e3bf5aae7f7aef9afbb5
Kennytaro <119624844+Kennytaro@users.noreply.github.com>
2026-04-14 21:18:34 -0500

    Update game.py
    Manual addition so it overwrite old code as my git pushes still having problems, but had updated code from waverly as well with audio

--- systems/game.py
+++ systems/game.py
@@ -4,6 +4,8 @@ from entities.enemy import spawn_hazards
 from entities.merch import spawn_merchs
 from systems.world import draw_world, draw_pause
 from systems.hud import draw_hud
+from systems.levels import get_level
+from entities.booths import get_booth_rects
 from systems.audio import AudioBank
 
 
@@ -11,39 +13,62 @@ class Game:
     fps = 60
 
     def __init__(self):
-        self.player = Player(WIDTH // 2, HEIGHT // 2)
+
+        self.player = Player(WIDTH//2,HEIGHT//2)
 
         self.merchs = []
         self.hazards = []
 
-        self.audio = AudioBank()
+        self.level = 0
+        self.level_data = get_level(self.level)
 
-        self.spawn_level()
+        self.audio = AudioBank()
 
         self.state = "title"
 
+        self.spawn_level()
+
+
     def spawn_level(self):
+
         self.merchs.clear()
         self.hazards.clear()
 
-        self.merchs = spawn_merchs(player_rect=self.player.rect)
-        self.hazards = spawn_hazards(self.merchs, player_rect=self.player.rect)
+        self.player.rect.center = (WIDTH//2, HEIGHT//2)
+        self.player.score = 0  # reset score each level
 
+        self.level_data = get_level(self.level)
+        self.booths = get_booth_rects(self.level)
+
+        self.merchs = spawn_merchs(player_rect = self.player.rect, count = self.level_data["merch_count"], level = self.level)
+        self.hazards = spawn_hazards(self.merchs, player_rect=self.player.rect, count = self.level_data["hazard_count"],  speed = self.level_data["hazard_speed"], booths = self.booths)
+    
     def update(self, dt):
+
         if self.state not in ("play",):
             return
 
-        self.player.update(dt)
+        self.player.update(dt, self.booths)
 
+        for hz in self.hazards:
+            hz.update(dt, self.hazards, self.booths)
+
+        #Merch collection
         for merch in self.merchs[:]:
             if self.player.rect.colliderect(merch.rect):
                 self.merchs.remove(merch)
                 self.player.score += 1
                 self.audio.play("ui_confirm")
 
-                if self.player.score >= 10:
-                    self.state = "win"
+                if self.player.score >= self.level_data["merch_count"]:
+                    self.level += 1
+                        # STOP AFTER LEVEL 2 (index 1)
+                    if self.level > 1:
+                        self.state = "win"
+                    else:
+                        self.spawn_level()
 
+        #Hazard damage + i-frame
         for hz in self.hazards:
             if self.player.rect.colliderect(hz.rect) and not self.player.is_invincible:
                 self.player.hp -= 10
@@ -54,11 +79,13 @@ class Game:
                     self.state = "gameover"
 
     def draw(self):
+
         screen.fill(GRAY)
 
         draw_hud(screen, self.player)
-        draw_world(screen)
+        draw_world(screen, self.level)
 
+        # world
         for merch in self.merchs:
             merch.draw(screen)
 
@@ -67,20 +94,22 @@ class Game:
 
         self.player.draw(screen)
 
+        # title screen
         if self.state == "title":
             t = FONT.render("Press SPACE to Start, Press Q to Quit", True, WHITE)
-            r = t.get_rect(center=(WIDTH // 2, HEIGHT // 2))
+            r = t.get_rect(center=(WIDTH//2, HEIGHT//2))
             screen.blit(t, r)
 
         if self.state == "gameover":
             t = FONT.render("Game Over - Press SPACE", True, WHITE)
-            r = t.get_rect(center=(WIDTH // 2, HEIGHT // 2))
+            r = t.get_rect(center=(WIDTH//2, HEIGHT//2))
             screen.blit(t, r)
 
         if self.state == "paused":
             draw_pause(screen)
 
         if self.state == "win":
-            t = FONT.render("You Win! Press SPACE to Play Again, or Q to Quit", True, WHITE)
-            r = t.get_rect(center=(WIDTH // 2, HEIGHT // 2))
+            t = FONT.render("You Win", True, WHITE)
+            r = t.get_rect(center=(WIDTH//2, HEIGHT//2))
             screen.blit(t, r)
+   
