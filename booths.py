614f4470e3d9db1d0f54ab95e20d063309461780
Kennytaro <119624844+Kennytaro@users.noreply.github.com>
2026-04-14 21:12:48 -0500

    Delete booths.py

--- booths.py
+++
@@ -1,8 +0,0 @@
-import pygame
-
-def get_booth_rects():
-    booth_rects = []
-    for x in range(100, 900, 200):
-        booth_rects.append(pygame.Rect(x, 150, 100, 60))
-        booth_rects.append(pygame.Rect(x, 350, 100, 60))
-    return booth_rects
