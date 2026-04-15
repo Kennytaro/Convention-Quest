17d38cbabd50b488d9648744a8661420b075b4ae
Kennytaro <119624844+Kennytaro@users.noreply.github.com>
2026-04-14 21:18:58 -0500

    Update booths.py

--- entities/booths.py
+++ entities/booths.py
@@ -1,8 +1,17 @@
 import pygame
 
-def get_booth_rects():
+from settings import WIDTH, HEIGHT, HUD_HEIGHT
+
+def get_booth_rects(level = 0):
     booth_rects = []
-    for x in range(100, 900, 200):
-        booth_rects.append(pygame.Rect(x, 150, 100, 60))
-        booth_rects.append(pygame.Rect(x, 350, 100, 60))
+    
+    if level == 0:
+        for x in range(100, 900, 200):
+            booth_rects.append(pygame.Rect(x, 150, 100, 60))
+            booth_rects.append(pygame.Rect(x, 350, 100, 60))
+    elif level == 1:
+        rows_y = [HUD_HEIGHT + 80, HEIGHT // 2, HEIGHT - 100]  # 3 evenly spaced lines
+        for y in rows_y:
+            for x in range(100, WIDTH - 100, 140):
+                booth_rects.append(pygame.Rect(x, y, 80, 60))
     return booth_rects
