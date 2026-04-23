fd689a2eb3a521654db12dc97939611c8a4d1f27
Kennytaro <119624844+Kennytaro@users.noreply.github.com>
2026-04-22 23:53:10 -0500

    Update hud.py

--- systems/hud.py
+++ systems/hud.py
@@ -6,17 +6,20 @@ def draw_hud(screen, player):
       # HUD
     pygame.draw.rect(screen, HUD,(0, 0, WIDTH, HUD_HEIGHT))
         
+    title_text = FONT.render("Convention Quest", True, WHITE)
+    title_rect = title_text.get_rect(center=(WIDTH // 2, HUD_HEIGHT // 2))   
+    screen.blit(title_text, title_rect)
     draw_health_bar(screen, 20, 200, 30, player.hp, player.max_hp)
 
     merch_text = FONT.render(f"Merch: {player.score}", True, WHITE)
     merch_rect = merch_text.get_rect(topright=(screen.get_width() - 20, 20))
         
     screen.blit(merch_text, merch_rect)
+    
 
 def draw_health_bar(screen, x, width, height, hp, max_hp):
 
     y = (HUD_HEIGHT - height) // 2
-
 
     # background (empty health)
     pygame.draw.rect(screen, GRAY, (x, y, width, height))
